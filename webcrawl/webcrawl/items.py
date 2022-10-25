c541f1693a7c76b8025c754114e2eb451795cbc7
Angelica Aluo <angelicaaluo@Angelicas-MacBook-Air.local>
2022-10-25 21:13:07 +0100

    updates files

--- webcrawl/webcrawl/items.py
+++ webcrawl/webcrawl/items.py
@@ -6,11 +6,19 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import MapCompose, TakeFirst
+from w3lib.html import remove_tags
 
+def remove_whitespace(value):
+    return value.strip()
 
-class WebcrawlItem(scrapy.Item):
-    # define the fields for your item here like:
-    name = scrapy.Field()
-    # weight = scrapy.Field()
-    price = scrapy.Field()
-    url = scrapy.Field()
+# 
+# class WebcrawlItem(scrapy.Item):
+#     # define the fields for your item here like:
+#     name = scrapy.Field()
+#     price = scrapy.Field()
+#     url = scrapy.Field()
+class webcrawlItem(scrapy.Item):
+    name = scrapy.Field(input_processor=MapCompose(remove_tags,remove_whitespace,'\n'),output_processor=TakeFirst(), )
+    price = scrapy.Field(input_processor=MapCompose(remove_tags,remove_whitespace,'\n'),output_processor=TakeFirst(), )
+    url = scrapy.Field(input_processor=MapCompose(remove_tags,remove_whitespace,'\n'),output_processor=TakeFirst(), )
--- webcrawl/webcrawl/spiders/web_crawl.py
+++ webcrawl/webcrawl/spiders/web_crawl.py
@@ -1,6 +1,6 @@
 #import scrapy
 import scrapy
-from webcrawl.items import WebcrawlItem
+from webcrawl.items import webcrawlItem
 
 class WebCrawl(scrapy.Spider):
     #the name of the spider
@@ -12,16 +12,17 @@ class WebCrawl(scrapy.Spider):
     start_urls = ['https://www.ocado.com/browse/value-just-for-you-323660/everyday-savers-323640?clkInTab=Everyday%20Savers'] 
 
     def parse(self, response):
-        web_item=WebcrawlItem()
+        web_item=webcrawlItem()
 
         #here we are looping thru the products and extracting the name price and url
         products = response.xpath("//div[@class='fop-contentWrapper']")
         for product in products:
             
-            web_item['name']= product.xpath("//h4[@class='fop-title']/span/text()").extract(),
+            web_item['name']= '\n'.join(product.xpath("//h4[@class='fop-title']/span/text()").extract()),
+            #print(web_item['name'])
             # web_item['weight']=product.xpath("//span[@class='fop-catch-weight-inline']/text()"),
-            web_item['price'] =product.xpath("//span[@class='fop-price fop-value-delivered price-offer']/text()").extract(),
-            web_item['url']= product.xpath("//div[@class='fop-contentWrapper']/a/@href").extract()
+            web_item['price'] ='\n'.join(product.xpath("//span[@class='fop-price fop-value-delivered price-offer']/text()").extract()),
+            web_item['url']= '\n'.join(product.xpath("//div[@class='fop-contentWrapper']/a/@href").extract())
 
         yield web_item
             
